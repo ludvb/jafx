429fcf6efc4034437a0774f36e4dab40f9e67e22
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2021-11-07 15:59:32 +0100

    include log_image code path in test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -14,20 +14,23 @@ from jafx.contrib.logging import (
     LogLevel,
     TensorboardLogger,
     TextLogger,
+    log_image,
     log_info,
     log_scalar,
 )
 from jafx.contrib.logging.text_logger import format_string, make_string_formatter
+from jafx.hparams import HParams
 from jafx.io import load_dynamic_state, save_dynamic_state
 from jafx.params import update_params
 
 
 def _compute_loss(z):
-    x = param("x", jnp.ones((3, 3)))
-    linear = wrap_haiku("linear", lambda x: hk.Linear(3)(x))
-    x = linear(x)
+    x = param("x", jnp.ones((10, 10, 1)))
+    conv = wrap_haiku("conv", lambda x: hk.Conv2D(3, 3)(x))
+    x = conv(x)
     log_info(format_string("x.sum() = {}", x.sum()))
-    log_scalar("x", x.sum(), log_frequency=5)
+    log_scalar("x_sum", x.sum(), log_frequency=5)
+    log_image("x_image", x, log_frequency=5)
     x = (z * x ** 2).sum()
     return x, x
 
@@ -45,8 +48,8 @@ def test_functional(tmp_path):
     loss = float("inf")
     with TextLogger([sys.stderr], log_level=LogLevel.DEBUG), make_string_formatter():
         with TensorboardLogger(tmp_path / "tb_logs"):
-            with default.handlers():
-                for _ in range(10):
+            with default.handlers(), HParams(default_lr=0.1):
+                for _ in range(100):
                     loss = _update_state(jnp.ones(jax.device_count()))
                     log_info("loss = " + str(loss.mean()))
 
