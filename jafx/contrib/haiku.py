e135156b79af741b29a533a8b8dff5177f989a1d
Ludvig Bergenstråhle <jlbergenstrahle@gmail.com>
2021-11-08 13:34:27 +0100

    run module_init only when there is no state and params

--- jafx/contrib/haiku.py
+++ jafx/contrib/haiku.py
@@ -6,6 +6,7 @@ import jax.numpy as jnp
 from jax.interpreters.batching import BatchTracer
 
 from .. import Namespace, param, state
+from ..params import NoParamException
 from ..prng import next_prng_key
 
 
@@ -35,29 +36,23 @@ def wrap_haiku(
     module_init, module_apply = hk.transform_with_state(fn)
 
     def _run_module(*args, **kwargs):
-        module_param_default, module_state_default = module_init(
-            next_prng_key(), *args, **kwargs
-        )
+        try:
+            with Namespace(scope=name):
+                module_state = state.get("haiku_state")
+            module_param = param(name)
 
-        module_param_default, module_param_treedef = jax.tree_util.tree_flatten(
-            module_param_default
-        )
-        module_param = param(name, module_param_default)
-        module_param = jax.tree_util.tree_unflatten(module_param_treedef, module_param)
+        except (state.StateException, NoParamException):
+            module_param_default, module_state = module_init(
+                next_prng_key(), *args, **kwargs
+            )
+            module_param = param(name, module_param_default)
 
         with Namespace(scope=name):
-            try:
-                module_state = state.get("haiku_state")
-            except state.StateException:
-                module_state = module_state_default
-
             result, new_state = module_apply(
                 module_param, module_state, next_prng_key(), *args, **kwargs
             )
-
             new_state = jax.lax.stop_gradient(new_state)
             new_state = jax.tree_util.tree_map(_state_batch_reduction_mapper, new_state)
-
             state.set("haiku_state", new_state)
 
         return result
