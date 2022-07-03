0787aac5b89b854dd0d2100915282ead0ad9c05e
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2022-07-03 17:32:21 +0200

    remove default hparams

--- jafx/default.py
+++ jafx/default.py
@@ -1,10 +1,7 @@
-from jax.example_libraries.optimizers import adam
-
 from .io import StateIO
 from .params import ParamHandler, opt_state_io_packer
 from .state import DynamicState, StaticState
 from .util import StackedContext
-from . import hparams
 
 
 def handlers():
@@ -13,6 +10,5 @@ def handlers():
         opt_state_packer=opt_state_io_packer,
         static_state=StaticState(),
         dynamic_state=DynamicState(),
-        hparams=hparams(optimizer=adam, learning_rate=1e-3),
         param_store=ParamHandler(),
     )
